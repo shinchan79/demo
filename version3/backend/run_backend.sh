09e7186f04c4d578432e7064e08c6f63b90f79cc
Yen Trinh <haiyentrinh07.09@gmail.com>
2021-03-26 10:28:50 +0700

    update

--- version3/backend/run_backend.sh
+++ version3/backend/run_backend.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 mongod --fork --logpath /var/log/mongodb/mongod.log && java -Dspring.data.mongodb.uri=mongodb://localhost:27017/todoapp -jar /home/spring-boot-backend/target/todoapp-0.0.1-SNAPSHOT.jar
